a1c0a370fa337ab3786440a75db3791c32d3fefe
Dennis Boldt <github@dennis-boldt.de>
2023-11-16 11:53:19 +0100

    Add grpc-web

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { EchoServiceClient } from './grpc/EchoServiceClientPb';
+import { EchoRequest } from './grpc/echo_pb';
 
 function App() {
+
+  const [echoResponse, setEchoResponse] = useState<EchoRequest.AsObject | undefined>(undefined);
+
+  useEffect(() => {
+    var echoService = new EchoServiceClient('http://localhost:8080');
+    
+    var request = new EchoRequest();
+    request.setMessage('Hello World!');
+
+    echoService.echo(request, {}, function(err, response) {
+      setEchoResponse(response.toObject());
+    });
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Response: {echoResponse ? echoResponse.message : "No response yet!"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
